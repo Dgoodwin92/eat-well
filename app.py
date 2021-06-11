73c1105a8c089cd7392069845b8c5c5638d13cb4
Dgoodwin92 <danielgoodwin85@gmail.com>
2021-06-11 17:15:30 +0000

    Submit function working for adding new recipe

--- app.py
+++ app.py
@@ -98,9 +98,23 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_recipe")
+@app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
-    return render_template("add-recipe.html")
+    if request.method == "POST":
+        task = {
+            "recipe_name": request.form.get("recipe_name"),
+            "category_name": request.form.get("category_name"),
+            "recipe_description": request.form.get("recipe_description"),
+            "ingredient_list": request.form.get("ingredient_list"),
+            "recipe_method": request.form.get("recipe_method"),
+            "serving_size": request.form.get("serving_size"),
+            "total_time":request.form.get("total_time")
+
+        }
+        mongo.db.recipes.insert_one(task)
+        flash("Recipe Succesfully Added")
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("add-recipe.html", categories=categories)
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,40 @@ $(document).ready(function(){
     $('.materialboxed').materialbox();
     $('select').formSelect();
   });
+
+$('#ingredients .add-ingredient-list-item').click(function (event) {
+  let IngredientItem = `<li class="collection-item">
+                                <div class="input-field">
+                                    <input name="ingredients" type="text" maxlength="100" pattern="^[A-Za-z0-9 ]*[A-Za-z0-9][A-Za-z0-9 ]*$" required>
+                                    <label for="ingredients">Ingredient</label>
+                                </div>
+                                <a class="remove-list-item">
+                                    <i class="fas fa-times"></i>
+                                    <span class="sr-only">Remove Ingredient</span>
+                                </a>
+                            </li>`;
+  $(this).parent().before(IngredientItem);
+});
+
+$('#ingredients').on("click", ".remove-list-item", function (event) {
+  $(this).parent().remove();
+});
+
+$('#method_step .add-method-step-item').click(function (event) {
+  let methodStep = `<li class="collection-item">
+                          <a class="remove-list-item">
+                              <i class="fas fa-times"></i>
+                              <span class="sr-only">Remove Method Step</span>
+                          </a>
+                          <div class="input-field">
+                          <textarea name="method_step" class="materialize-textarea" required></textarea>
+                          <label for="method_step">Step Description</label>
+                          </div>
+                      </li>`;
+  $(this).parent().before(methodStep);
+});
+
+// Remove Method step item on click
+$('#method_step').on("click", ".remove-list-item", function (event) {
+  $(this).parent().remove();
+});
